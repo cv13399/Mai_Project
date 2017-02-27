774d96af160fdb8338e801902bd3bd740d257352
Your Name <cv13399@gmail.com>
2017-02-27 23:36:01 +0800

    push up project

---
+++ Mai_project/Assets/MyUIManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyUIManager : MonoBehaviour 
+{
+    public  UILabel loadingLabel;
+    public  UIPanel loadingPanel;
+    public UIEventListener LoginBtn;
+	// Use this for initialization
+	void Start () 
+    {
+        ButtonEventInitialize();
+	}
+    void ButtonEventInitialize()
+    {
+        LoginBtn.onClick += (GameObject g) =>
+        {
+            print("Login");
+        };
+    }
+
+    public IEnumerator LoadingAnimation()
+    {
+        loadingPanel.gameObject.SetActive(true);
+        string[] text = new string[] { "Loading", "Loading.", "Loading..", "Loading..." };
+        for (int i = 0; i < text.Length; i++)
+        {
+            loadingLabel.text = text[i];
+            yield return new WaitForSeconds(0.5f);      
+        }
+        loadingPanel.gameObject.SetActive(false);
+    }
+}
